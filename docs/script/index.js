005f961b3039609f1e1604fe0831ffee3db01540
manyrice <alsgml1966@naver.com>
2021-09-11 19:24:18 +0900

    sns로고 추가 , circle 애니메이션 실패

--- docs/script/index.js
+++ docs/script/index.js
@@ -11,91 +11,128 @@ $("body").imagesLoaded(function () {
   }, fakeLoadingMilliSeconds);
 });
 
-
+// 조절가능
+let chart1AllowRerender = false;
+// 건들지 마시오
+let chart1Rendered = false;
 // 풀페이지
 function Fullpage__init() {
   new fullpage('#fullpage', {
-    scrollOverflow:true,
-    verticalCentered:false,
-    css3:false,
-    navigation:true,
+    scrollOverflow: true,
+    verticalCentered: false,
+    css3: false,
+    navigation: true,
     scrollingSpeed: 1000,
-    //인터넷에서 찾은..텍스트 커서 잡히기?
+    //인터넷에서 찾은..텍스트 커서 잡히기
     scrollOverflowOptions: {
-    	preventDefault: false
+      preventDefault: false
     },
-    menu: ".top-bar .menu-box-1 > ul"
+    menu: ".top-bar .menu-box-1 > ul",
+    afterLoad: function(ignored, destination){
+      if ( destination.anchor == 'section-3') {
+        $(".chart-1").circleProgress({value:0.9});
+      }
+    },
   });
+
+
+  $(".chart-1")
+    .circleProgress({
+      startAngle: -Math.PI / 2,
+      reverse: true,
+      value: 0.1,
+      lineCap: 'round',
+      thickness: 15,
+      fill: {
+        gradient: ['#ff1e41', '#ff9f8e']
+      }
+    })
+    .on(
+      "circle-animation-progress",
+      function (event, progress, stepValue) {
+        $(this).find("strong").text(parseInt(stepValue * 100) + '%');
+      }
+    );
 
   // 기존 섹션을 다시 활성화 하여, 최초에도 애니메이션이 발동 하도록
   const $current = $('#fullpage .section.fp-section.active');
   $current.removeClass('active');
-  setTimeout(function() {
+  setTimeout(function () {
     $current.addClass('active');
   });
 }
-Fullpage__init();
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 메인화면 마우스움직임 효과
 function MainMouseMoveEffect() {
 
 
-const $window = $(window);
+  const $window = $(window);
 
-let windowWidth = $window.width();
-let windowHeight = $window.height();
+  let windowWidth = $window.width();
+  let windowHeight = $window.height();
 
-$window.resize(_.throttle(function () {
-  windowWidth = $window.width();
-  windowHeight = $window.height();
-}, 100));
+  $window.resize(_.throttle(function () {
+    windowWidth = $window.width();
+    windowHeight = $window.height();
+  }, 100));
 
-$window.resize(_.throttle(function () {
-  MousemoveEffect1__update();
-}, 100));
-
-let MousemoveEffect1__$el = null;
-let MousemoveEffect1__lastPosX = 0;
-let MousemoveEffect1__lastPosY = 0;
-
-function MousemoveEffect1__update() {
-  MousemoveEffect1__$el.each(function (index, node) {
-    const $node = $(node);
-    const horRes = $node.data('data-mousemove-effect1-hor-res');
-    const verRes = $node.data('data-mousemove-effect1-ver-res');
-    
-    const x = (MousemoveEffect1__lastPosX - (windowWidth / 2)) * horRes;
-    const y = (MousemoveEffect1__lastPosY - (windowHeight / 2)) * verRes;
-    $(node).css('transform', 'translateX(' + x + 'px) translateY(' + y + 'px)');
-    
-    console.log("MousemoveEffect1__lastPosX : " + MousemoveEffect1__lastPosX);
-    console.log("MousemoveEffect1__lastPosY : " + MousemoveEffect1__lastPosY);
-  });
-}
-
-function MousemoveEffect1__init() {
-  MousemoveEffect1__$el = $('.mousemove-effect-1-el');
-
-  MousemoveEffect1__$el.each(function (index, node) {
-    const $node = $(node);
-    $node.data('data-mousemove-effect1-hor-res', $node.attr('data-mousemove-effect1-hor-res') * 1);
-    $node.data('data-mousemove-effect1-ver-res', $node.attr('data-mousemove-effect1-ver-res') * 1);
-  });
-
-  const MousemoveEffect1__updateThrottled = _.throttle(function () {
+  $window.resize(_.throttle(function () {
     MousemoveEffect1__update();
-  }, 10);
+  }, 100));
 
-  $window.mousemove(function (e) {
-    MousemoveEffect1__lastPosX = e.clientX;
-    MousemoveEffect1__lastPosY = e.clientY;
+  let MousemoveEffect1__$el = null;
+  let MousemoveEffect1__lastPosX = 0;
+  let MousemoveEffect1__lastPosY = 0;
 
-    MousemoveEffect1__updateThrottled();
-  });
-}
+  function MousemoveEffect1__update() {
+    MousemoveEffect1__$el.each(function (index, node) {
+      const $node = $(node);
+      const horRes = $node.data('data-mousemove-effect1-hor-res');
+      const verRes = $node.data('data-mousemove-effect1-ver-res');
 
-MousemoveEffect1__init();
+      const x = (MousemoveEffect1__lastPosX - (windowWidth / 2)) * horRes;
+      const y = (MousemoveEffect1__lastPosY - (windowHeight / 2)) * verRes;
+      $(node).css('transform', 'translateX(' + x + 'px) translateY(' + y + 'px)');
+
+      console.log("MousemoveEffect1__lastPosX : " + MousemoveEffect1__lastPosX);
+      console.log("MousemoveEffect1__lastPosY : " + MousemoveEffect1__lastPosY);
+    });
+  }
+
+  function MousemoveEffect1__init() {
+    MousemoveEffect1__$el = $('.mousemove-effect-1-el');
+
+    MousemoveEffect1__$el.each(function (index, node) {
+      const $node = $(node);
+      $node.data('data-mousemove-effect1-hor-res', $node.attr('data-mousemove-effect1-hor-res') * 1);
+      $node.data('data-mousemove-effect1-ver-res', $node.attr('data-mousemove-effect1-ver-res') * 1);
+    });
+
+    const MousemoveEffect1__updateThrottled = _.throttle(function () {
+      MousemoveEffect1__update();
+    }, 10);
+
+    $window.mousemove(function (e) {
+      MousemoveEffect1__lastPosX = e.clientX;
+      MousemoveEffect1__lastPosY = e.clientY;
+
+      MousemoveEffect1__updateThrottled();
+    });
+  }
+
+  MousemoveEffect1__init();
 };
 MainMouseMoveEffect();
 
@@ -109,7 +146,7 @@ function PortfolioSlider__init() {
       prevEl: ".btn-prev",
     },
     on: {
-      slideChange: function() {
+      slideChange: function () {
         $('.pf-slide').attr('data-swiper-slide-index', this.realIndex);
       }
     }
@@ -121,11 +158,11 @@ PortfolioSlider__init();
 //아트웍 스와이퍼 슬라이드
 function ArtworkSlider__init() {
   const swiper = new Swiper('.aw-slide .swiper-container', {
-    slidesPerView:4.5,
-    spaceBetween:50,
+    slidesPerView: 4.5,
+    spaceBetween: 50,
     loop: false,
     on: {
-      slideChange: function() {
+      slideChange: function () {
         $('.aw-slide').attr('data-swiper-slide-index', this.realIndex);
       }
     }
