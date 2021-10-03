a0223fde68aa1b9c61edcd909d3e895c1dc5ec5c
manyrice <alsgml1966@naver.com>
2021-10-04 04:18:18 +0900

    form 입력

--- docs/script/index.js
+++ docs/script/index.js
@@ -1,5 +1,25 @@
 console.clear();
 
+function sendEmailForm(form) {
+  if ( form._replyto.value.length == 0 ) {
+    alert('이메일 주소를 입력해주세요.');
+    form._replyto.focus();
+    return;
+  }
+  
+  if ( form.message.value.length == 0 ) {
+    alert('메세지를 입력해주세요.');
+    form.message.focus();
+    return;
+  }
+  
+  form.submit();
+  
+  form._replyto.value = '';
+  form.message.value = '';
+  form.submit.innerHTML = '전송되었습니다.';
+  form.submit.disabled = true;
+}
 
 // 로딩화면
 $("body").imagesLoaded(function () {
